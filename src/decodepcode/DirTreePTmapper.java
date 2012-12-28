85c69ae91f8fcf1bad9427636a3729c4dd494f0e
erikh3 <erikh3@d05c7767-a8a5-47bd-bb89-816ecc5280c6>
2012-12-28 00:04:13 +0000

    0.46 - Filter out certain characters (*?/<>) when combining keys to path (either file path or path for SVN).
    git-svn-id: https://svn.code.sf.net/p/decodepcode/code/trunk@64 d05c7767-a8a5-47bd-bb89-816ecc5280c6

--- src/decodepcode/DirTreePTmapper.java
+++ src/decodepcode/DirTreePTmapper.java
@@ -11,6 +11,12 @@ public class DirTreePTmapper implements PToolsObjectToFileMapper {
 	{
 		rootDir = _rootDir;
 	}
+	static String filterFileName( String fname)
+	{
+		if (fname == null)
+			return null;
+		return fname.trim().replace("/", "_").replace("?", "_").replace("*", "_") .replace("<", "_lt_").replace(">", "_gt_");
+	}
 	public File getFile(PeopleToolsObject obj, String extension)
 			throws IOException 
 	{
@@ -22,14 +28,13 @@ public class DirTreePTmapper implements PToolsObjectToFileMapper {
 				last = i;
 		for (int i = 0; i < last; i++)
 		{			
-			String dirName = obj.getKeys()[i].trim().replace("/", "_").replace("?", "_").replace("*", "_");
+			String dirName = filterFileName(obj.getKeys()[i]);
 			f = new File(f, dirName);
 		}	
 		f.mkdirs();
-		f = new File(f, obj.getKeys()[last].trim().replace("/", "_").replace("?", "_").replace("*", "_") + "." + extension);
+		f = new File(f, filterFileName(obj.getKeys()[last]));
 		return f;
-	}
-	
+	}	
 	public File getFileForSQL(SQLobject sqlObject, String extension) throws IOException {
 		String f1;
 		if (sqlObject.sqlType == 2)
@@ -47,9 +52,9 @@ public class DirTreePTmapper implements PToolsObjectToFileMapper {
 						f1 = "SQL" + sqlObject.sqlType;
 		File f = new File(rootDir, f1);
 		for ( int i = 0; i < sqlObject.getKeys().length; i++) // optional MARKET and/or DBTYPE
-			f = new File(f, sqlObject.getKeys()[i]); 
+			f = new File(f, filterFileName(sqlObject.getKeys()[i])); 
 		f.mkdirs();
-		return new File(f, sqlObject.getKeys()[0]+ "." + extension);
+		return new File(f, filterFileName(sqlObject.getKeys()[0])+ "." + extension);
 	}
 	public String getPath(PeopleToolsObject obj, String extension) {
 		String pcType = JDBCPeopleCodeContainer.objectTypeStr(obj.getPeopleCodeType());
@@ -59,8 +64,8 @@ public class DirTreePTmapper implements PToolsObjectToFileMapper {
 			if (obj.getKeys()[i] != null && obj.getKeys()[i].trim().length() > 0)
 				last = i;
 		for (int i = 0; i <= last; i++)
-			f += obj.getKeys()[i].trim() + "/";
-		f += obj.getKeys()[last].trim() + "." + extension;
+			f += filterFileName(obj.getKeys()[i]) + "/";
+		f += filterFileName(obj.getKeys()[last]) + "." + extension;
 		return f;
 	}
 	public String getPathForSQL(SQLobject sqlObject, String extension) {
@@ -81,9 +86,8 @@ public class DirTreePTmapper implements PToolsObjectToFileMapper {
 						else
 							f = "/SQL" + sqlObject.sqlType;
 			for ( int i = 0; i < sqlObject.getKeys().length; i++) // optional MARKET and/or DBTYPE
-				f = f + "/" + sqlObject.getKeys()[i]; 
-			return f + "/" + sqlObject.getKeys()[0]+ "." + extension;
+				f = f + "/" + filterFileName(sqlObject.getKeys()[i]); 
+			return f + "/" + filterFileName(sqlObject.getKeys()[0])+ "." + extension;
 		
 	}
-
 }
