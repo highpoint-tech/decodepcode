c38d60a32fc2f94aa7ee75846668beb5b5eb641a
erikh3 <erikh3@d05c7767-a8a5-47bd-bb89-816ecc5280c6>
2016-10-01 03:15:49 +0000

    Fix character code problem
    git-svn-id: https://svn.code.sf.net/p/decodepcode/code/trunk@101 d05c7767-a8a5-47bd-bb89-816ecc5280c6

--- src/decodepcode/compares/ExtractPeopleCodeFromCompareReport.java
+++ src/decodepcode/compares/ExtractPeopleCodeFromCompareReport.java
@@ -53,31 +53,32 @@ public class ExtractPeopleCodeFromCompareReport
 	
 	private static HashMap<String,String> htmlEntities;
 	  static {
-		    htmlEntities = new HashMap<String,String>();
-		    htmlEntities.put("&lt;","<")    ; htmlEntities.put("&gt;",">");
-		    htmlEntities.put("&amp;","&")   ; htmlEntities.put("&quot;","\"");
-		    htmlEntities.put("&agrave;","�"); htmlEntities.put("&Agrave;","�");
-		    htmlEntities.put("&acirc;","�") ; htmlEntities.put("&auml;","�");
-		    htmlEntities.put("&Auml;","�")  ; htmlEntities.put("&Acirc;","�");
-		    htmlEntities.put("&aring;","�") ; htmlEntities.put("&Aring;","�");
-		    htmlEntities.put("&aelig;","�") ; htmlEntities.put("&AElig;","�" );
-		    htmlEntities.put("&ccedil;","�"); htmlEntities.put("&Ccedil;","�");
-		    htmlEntities.put("&eacute;","�"); htmlEntities.put("&Eacute;","�" );
-		    htmlEntities.put("&egrave;","�"); htmlEntities.put("&Egrave;","�");
-		    htmlEntities.put("&ecirc;","�") ; htmlEntities.put("&Ecirc;","�");
-		    htmlEntities.put("&euml;","�")  ; htmlEntities.put("&Euml;","�");
-		    htmlEntities.put("&iuml;","�")  ; htmlEntities.put("&Iuml;","�");
-		    htmlEntities.put("&ocirc;","�") ; htmlEntities.put("&Ocirc;","�");
-		    htmlEntities.put("&ouml;","�")  ; htmlEntities.put("&Ouml;","�");
-		    htmlEntities.put("&oslash;","�") ; htmlEntities.put("&Oslash;","�");
-		    htmlEntities.put("&szlig;","�") ; htmlEntities.put("&ugrave;","�");
-		    htmlEntities.put("&Ugrave;","�"); htmlEntities.put("&ucirc;","�");
-		    htmlEntities.put("&Ucirc;","�") ; htmlEntities.put("&uuml;","�");
-		    htmlEntities.put("&Uuml;","�")  ; htmlEntities.put("&nbsp;"," ");
-		     htmlEntities.put("&apos;","'");
-		    
-		    htmlEntities.put("&euro;","\u20a0");
-		  }
+	    htmlEntities = new HashMap<String,String>();
+	    htmlEntities.put("&lt;","<")    ; htmlEntities.put("&gt;",">");
+	    htmlEntities.put("&amp;","&")   ; htmlEntities.put("&quot;","\"");
+	    htmlEntities.put("&agrave;","à"); htmlEntities.put("&Agrave;","À");
+	    htmlEntities.put("&acirc;","â") ; htmlEntities.put("&auml;","ä");
+	    htmlEntities.put("&Auml;","Ä")  ; htmlEntities.put("&Acirc;","Â");
+	    htmlEntities.put("&aring;","å") ; htmlEntities.put("&Aring;","Å");
+	    htmlEntities.put("&aelig;","æ") ; htmlEntities.put("&AElig;","Æ" );
+	    htmlEntities.put("&ccedil;","ç"); htmlEntities.put("&Ccedil;","Ç");
+	    htmlEntities.put("&eacute;","é"); htmlEntities.put("&Eacute;","É" );
+	    htmlEntities.put("&egrave;","è"); htmlEntities.put("&Egrave;","È");
+	    htmlEntities.put("&ecirc;","ê") ; htmlEntities.put("&Ecirc;","Ê");
+	    htmlEntities.put("&euml;","ë")  ; htmlEntities.put("&Euml;","Ë");
+	    htmlEntities.put("&iuml;","ï")  ; htmlEntities.put("&Iuml;","Ï");
+	    htmlEntities.put("&ocirc;","ô") ; htmlEntities.put("&Ocirc;","Ô");
+	    htmlEntities.put("&ouml;","ö")  ; htmlEntities.put("&Ouml;","Ö");
+	    htmlEntities.put("&oslash;","ø") ; htmlEntities.put("&Oslash;","Ø");
+	    htmlEntities.put("&szlig;","ß") ; htmlEntities.put("&ugrave;","ù");
+	    htmlEntities.put("&Ugrave;","Ù"); htmlEntities.put("&ucirc;","û");
+	    htmlEntities.put("&Ucirc;","Û") ; htmlEntities.put("&uuml;","ü");
+	    htmlEntities.put("&Uuml;","Ü")  ; htmlEntities.put("&nbsp;"," ");
+	    htmlEntities.put("&copy;","\u00a9");
+	    htmlEntities.put("&reg;","\u00ae");
+	    htmlEntities.put("&euro;","\u20a0");
+	  }
+	  
 	  public static final String unescapeHTML(String source) {
 	      int i, j;
 
